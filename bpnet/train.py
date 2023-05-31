e7f7112158cadf28410827e6dcf44b58a90b7b48
hongyuezhilian <7924503+hongyuezhilian@user.noreply.gitee.com>
2023-06-01 00:27:59 +0800

    v1模型发布

--- bpnet/train.py
+++ bpnet/train.py
@@ -3,10 +3,13 @@ import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
+
+from tensorflow.keras.optimizers import SGD
 from tensorflow.keras import regularizers
 
 
@@ -29,21 +32,27 @@ y_scaler = MinMaxScaler(feature_range=(-1, 1))
 x = x_scaler.fit_transform(x)
 y = y_scaler.fit_transform(y)
 
+x, x_test, y, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
+
 # 定义神经网络模型
 model = Sequential()
-model.add(Dense(16, activation='relu', input_shape=(8,)))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(512, activation='relu'))
-model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu', input_shape=(8,)))
 model.add(Dense(32, activation='relu'))
+model.add(Dense(8, activation='relu'))
+# model.add(Dense(512, activation='relu'))
+# model.add(Dense(16, activation='relu'))
 model.add(Dense(1, activation='linear'))
 
 # 误差记录
-optimizer = Adam(lr=0.001)
+# optimizer = Adam(lr=0.003)
+optimizer=SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+# model.compile(optimizer=optimizer, loss='mse')
 model.compile(optimizer=optimizer, loss='mse')
+# model.compile(loss='categorical_crossentropy', optimizer=sgd, class_mode='categorical')
 
 # 训练模型
 history = model.fit(x, y, epochs=1000, batch_size=64)
+
 
 # 评估模型
 mse = model.evaluate(x, y)
@@ -63,12 +72,17 @@ plt.xlabel("循环次数")
 plt.show()
 
 # 预测值输出
-y_pred = model.predict(x)
+y_pred = model.predict(x_test)
+
+mse = model.evaluate(x_test, y_test)
+print('Validation MSE:', mse)
 
 # 预测值反归一化
-y = y_scaler.inverse_transform(y)
+y_test = y_scaler.inverse_transform(y_test)
 y_pred = y_scaler.inverse_transform(y_pred)
 print("the prediction is:", y_pred)
+
+
 
 # 将预测值存储到Excel表中
 df_out = pd.DataFrame(y_pred, columns=['Prediction'])
@@ -78,8 +92,11 @@ df_out.to_excel('prediction.xlsx', index=False)
 # 设置中文显示和解决负号显示问题
 plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
-plt.scatter(y, y_pred)
+plt.scatter(y_test, y_pred)
 plt.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
 plt.xlabel("实际值")
 plt.ylabel("预测值")
 plt.show()
+
+mse = model.evaluate(x_test, y_test)
+print('Validation MSE:', mse)
